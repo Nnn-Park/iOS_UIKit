809079607e97c3832982b034c7425916de54c362
Nnn <jaehyung1299@naver.com>
2024-05-22 02:32:19 +0900

    BMI계산 기능 및 버튼실행 기능 구현

--- BMICalculator/BMICalculator/ViewController.swift
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet var mainTitle: UILabel!
     @IBOutlet var subTitle: UILabel!
@@ -27,7 +27,8 @@ class ViewController: UIViewController {
     @IBOutlet var randomCalculateButton: UIButton!
     @IBOutlet var resultButton: UIButton!
     
-    
+    var height = 0
+    var weight = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,7 +50,7 @@ class ViewController: UIViewController {
         
         weightLabel.text = "몸무게는 어떻게 되시나요?"
         weightLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
-
+        
         weightLabelTextField.layer.cornerRadius = 15
         weightLabelTextField.layer.borderWidth = 2
         
@@ -60,13 +61,53 @@ class ViewController: UIViewController {
         resultButton.backgroundColor = .purple
         resultButton.layer.cornerRadius = 15
     }
-
+    
+    @IBAction func keyBoardDismiss(_ sender: UITapGestureRecognizer) {
+        
+        view.endEditing(true)
+    }
     
     
+    @IBAction func heightTextFieldEditingDidEnd(_ sender: UITextField) {
+        
+        if sender.text != nil {
+            height = Int(sender.text!)!
+        } else {
+            height = 0
+        }
+        print(height)
+    }
     
     
+    @IBAction func weightTextFieldEditingDidEnd(_ sender: UITextField) {
+        
+        if sender.text != nil {
+            weight = Int(sender.text!)!
+        } else {
+            weight = 0
+        }
+        print(weight)
+    }
     
     
-
+    @IBAction func resultButtonTapped(_ sender: UIButton) {
+        let bmiResult = calculateBMI(height, weight)
+        let formattedBMI = String(format: "%.1f", bmiResult)
+        let alret = UIAlertController(title: "당신의 BMI는 \(formattedBMI)입니다.", message: "", preferredStyle: .alert)
+        let yes = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alret.addAction(yes)
+        
+        present(alret, animated: true, completion: nil)
+    }
+    
+    func calculateBMI(_ height: Int, _ weight: Int) -> Double {
+        let heightInMeters = Double(height) / 100.0
+        let bmiResult = Double(weight) / (heightInMeters * heightInMeters)
+        print(bmiResult)
+        return bmiResult
+    }
+    
+    
 }
 
