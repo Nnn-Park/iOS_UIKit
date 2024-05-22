ba17d0b04123118c65e9400c1e5b392932e30049
Nnn <jaehyung1299@naver.com>
2024-05-22 20:42:10 +0900

    UserDefault로 BMI 결과 저장 및 삭제

--- BMICalculator/BMICalculator/ViewController.swift
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     @IBOutlet var mainImageView: UIImageView!
     
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var nicknameTextField: UITextField!
     
     @IBOutlet var heightLabel: UILabel!
     @IBOutlet var heightTextField: UITextField!
@@ -26,12 +28,16 @@ class ViewController: UIViewController {
     
     @IBOutlet var randomCalculateButton: UIButton!
     @IBOutlet var resultButton: UIButton!
+    @IBOutlet var recentresultButton: UIButton!
+    @IBOutlet var removeRecentResultButton: UIButton!
     
     var height = 0
     var weight = 0
+    var nickname = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         mainImageView.image = UIImage(named: "image")
         
@@ -41,6 +47,12 @@ class ViewController: UIViewController {
         subTitle.text = "당신의 BMI 지수를\n알려드릴께요."
         subTitle.numberOfLines = 2
         subTitle.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        
+        nicknameLabel.text = "닉네임을 적어주세요."
+        nicknameLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        
+        nicknameTextField.layer.cornerRadius = 15
+        nicknameTextField.layer.borderWidth = 2
         
         heightLabel.text = "키가 어떻게 되시나요?"
         heightLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
@@ -60,6 +72,12 @@ class ViewController: UIViewController {
         resultButton.setTitle("결과 확인", for: .normal)
         resultButton.backgroundColor = .purple
         resultButton.layer.cornerRadius = 15
+        
+        recentresultButton.setTitle("최신 결과 확인", for: .normal)
+        recentresultButton.setTitleColor(.lightGray, for: .normal)
+        
+        removeRecentResultButton.setTitle("최신 결과 삭제", for: .normal)
+        removeRecentResultButton.setTitleColor(.lightGray, for: .normal)
     }
     
     @IBAction func keyBoardDismiss(_ sender: UITapGestureRecognizer) {
@@ -76,6 +94,7 @@ class ViewController: UIViewController {
             height = 0
         }
         print(height)
+        UserDefaults.standard.set(height, forKey: "height")
     }
     
     
@@ -87,7 +106,18 @@ class ViewController: UIViewController {
             weight = 0
         }
         print(weight)
+        UserDefaults.standard.set(weight, forKey: "weight")
     }
+    
+    @IBAction func nicknameTextFieldEditingDidEnd(_ sender: UITextField) {
+        
+        if let name = sender.text {
+            nickname = name
+        }
+        print(nickname)
+        UserDefaults.standard.set(nickname, forKey: "nickname")
+    }
+    
     
     
     @IBAction func randomCalculateButtonDidTapped(_ sender: UIButton) {
@@ -101,10 +131,22 @@ class ViewController: UIViewController {
         alertResult()
     }
     
+    @IBAction func recentresultButtonTapped(_ sender: UIButton) {
+        
+        alertRecentResult()
+    }
+    
+    @IBAction func removeRecentresultButtonTapped(_ sender: UIButton) {
+        
+        alertRemoveRecentResult()
+    }
+    
+    
     func calculateBMI(_ height: Int, _ weight: Int) -> Double {
         let heightInMeters = Double(height) / 100.0
         let bmiResult = Double(weight) / (heightInMeters * heightInMeters)
         print(bmiResult)
+        UserDefaults.standard.set(bmiResult, forKey: "BMIResult")
         return bmiResult
     }
     
@@ -127,7 +169,7 @@ class ViewController: UIViewController {
         
         let bmiResult = calculateBMI(height, weight)
         let formattedBMI = String(format: "%.1f", bmiResult)
-        let alret = UIAlertController(title: "당신의 BMI는 \(formattedBMI)입니다.", message: "", preferredStyle: .alert)
+        let alret = UIAlertController(title: "\(nickname)님의 BMI는 \(formattedBMI)입니다.", message: "", preferredStyle: .alert)
         let yes = UIAlertAction(title: "확인", style: .default, handler: nil)
         
         alret.addAction(yes)
@@ -139,7 +181,7 @@ class ViewController: UIViewController {
         
         let bmiResult = calculateBMI(makeRandomHeight(), makeRandomWeight())
         let formattedBMI = String(format: "%.1f", bmiResult)
-        let alret = UIAlertController(title: "당신의 BMI는 \(formattedBMI)입니다.", message: "", preferredStyle: .alert)
+        let alret = UIAlertController(title: "랜덤 BMI는 \(formattedBMI)입니다.", message: "", preferredStyle: .alert)
         let yes = UIAlertAction(title: "확인", style: .default, handler: nil)
         
         alret.addAction(yes)
@@ -147,6 +189,26 @@ class ViewController: UIViewController {
         present(alret, animated: true, completion: nil)
     }
     
+    func alertRecentResult() {
+        let bmiResult = UserDefaults.standard.double(forKey: "BMIResult")
+        let formattedBMI = String(format: "%.1f", bmiResult)
+        let alret = UIAlertController(title: "가장 최근의 BMI는 \(formattedBMI)입니다.", message: "", preferredStyle: .alert)
+        let yes = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alret.addAction(yes)
+        
+        present(alret, animated: true, completion: nil)
+    }
+    
+    func alertRemoveRecentResult() {
+        UserDefaults.standard.removeObject(forKey: "BMIResult")
+        let alret = UIAlertController(title: "가장 최근의 BMI결과가 삭제되었습니다.", message: "", preferredStyle: .alert)
+        let yes = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alret.addAction(yes)
+        
+        present(alret, animated: true, completion: nil)
+    }
     
 }
 
