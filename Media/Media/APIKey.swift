d95ccf59c264749f32158979eeb29fc2261a2f02
Nnn <jaehyung1299@naver.com>
2024-06-10 21:02:13 +0900

    API 통신 및 레이아웃 구현

---
+++ Media/Media/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  Media
+//
+//  Created by 박재형 on 6/10/24.
+//
+
+import Foundation
+
+struct APIKey {
+    
+    static let movieURL = "https://api.themoviedb.org/3/trending/movie/{time_window}"
+}
